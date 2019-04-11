25b1606db85d12a0504ac25de8d966fbd2c5748b
ssvictoro@SALUD.GOB.AR <ssvictoro@SALUD.GOB.AR>
2019-04-11 15:11:44 -0300

    Herencia de vista y modelo

--- models/models.py
+++ models/models.py
@@ -37,3 +37,13 @@ class Session(models.Model):
     course_id = fields.Many2one('openacademy.course',
         ondelete='cascade', string="Course", required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
+
+class Partner(models.Model):
+    _inherit = 'res.partner'
+
+    # Add a new column to the res.partner model, by default partners are not
+    # instructors
+    instructor = fields.Boolean("Instructor", default=False)
+
+    session_ids = fields.Many2many('openacademy.session',
+        string="Attended Sessions", readonly=True)
